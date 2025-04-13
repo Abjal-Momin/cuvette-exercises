3e0db317bcf6e9399466d9831dba238e787ed5f1
Abjal Momin <afzalmomin818@gmail.com>
2025-04-13 22:28:06 +0530

    basic structure done

---
+++ 3-zomato-landingPage/test.js
@@ -0,0 +1,36 @@
+// let target = 1000;
+// const speed = 10;
+// for (let ini = 0; ini <= target; ini += speed) {
+//   function updte() {
+//     console.log(ini);
+//   }
+//   setTimeout(updte, 100);
+// }
+// function iife, function return funct call once
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// const arr = [1, 2, 3, 4];
+// const arr2 = [1, 2];
+
+// function hello() {
+//   console.log("hello");
+// }
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// arr.__proto__.push(hello);
+// Array.prototype.push(hello);
+
+// arr.__proto__ = hello;
+
+// console.log(arr2);
+
+// console.log(Array.prototype[0]);
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+
