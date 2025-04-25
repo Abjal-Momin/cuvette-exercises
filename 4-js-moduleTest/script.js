ae9a9bc36e5eddb101af97848dcd3d28bacb223a
Abjal Momin <afzalmomin818@gmail.com>
2025-04-25 12:30:41 +0530

    random logic is added

--- 4-js-moduleTest/script.js
+++ 4-js-moduleTest/script.js
@@ -1,9 +1,16 @@
 const cancel = document.getElementsByClassName("cross");
 const rule = document.querySelector(".rule");
-const userClick = document.querySelector(".secondSection");
 const gameBegin = document.querySelector(".mainContainer");
+
+const winStatus = document.querySelector(".WinnerStatus");
+const againstStatus = document.querySelector(".againstPC");
+
+const userClick = document.querySelector(".secondSection");
+const pcClick = document.querySelector(".pcSection");
 const imagePathUser = document.querySelector(".imgPathUser");
-const imageBorder = document.querySelector("#SecondCirculeUser");
+const imagePathPc = document.querySelector(".imgPathPc");
+const imageBorderUser = document.querySelector("#SecondCirculeUser");
+const imageBorderPc = document.querySelector("#SecondCirculePc");
 
 // To show Rules section
 function rules() {
@@ -16,8 +23,31 @@ function cross() {
 
 //User Choise to get the user Pick
 function userChoise(num, style) {
-  imageBorder.className = style;
+  rule.style.display = "none";
+
+  const randomNum = Math.floor(Math.random() * 3) + 1;
+  //Image Part
   imagePathUser.src = "./images/hand" + num + ".png";
+  imagePathPc.src = "./images/hand" + randomNum + ".png";
+
+  //For Styling part
+  imageBorderUser.className = style;
+  if (randomNum == "1") {
+    imageBorderPc.className = "stone";
+  } else if (randomNum == "2") {
+    imageBorderPc.className = "scissor";
+  } else {
+    imageBorderPc.className = "paper";
+  }
+
+  if (num < randomNum && randomNum == "2") {
+    winStatus.innerHTML = "YOU LOST";
+  } else if (num > randomNum && ) {
+  } else if (num == randomNum) {
+    winStatus.innerHTML = "TIE UP";
+    againstStatus.innerHTML = " ";
+  }
+
   userClick.style.display = "flex";
   gameBegin.style.display = "none";
 }
